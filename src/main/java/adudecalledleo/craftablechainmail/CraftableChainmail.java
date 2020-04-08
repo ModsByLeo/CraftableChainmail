73cedaf92a10597d01bf3abc2686ffd1c2d47fb7
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2020-04-08 19:48:29 +0300

    fix chainmail repair material replacing

--- src/main/java/adudecalledleo/craftablechainmail/CraftableChainmail.java
+++ src/main/java/adudecalledleo/craftablechainmail/CraftableChainmail.java
@@ -5,19 +5,18 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.LazyLoadBase;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
-import net.minecraftforge.registries.IForgeRegistryEntry;
-import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -27,13 +26,16 @@ public class CraftableChainmail {
 
     static final Logger LOGGER = LogManager.getLogger();
 
-    @ObjectHolder(MODID)
-    public static final Item CHAINMAIL_PLATE = null;
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MODID);
+
+    public static final RegistryObject<Item> CHAINMAIL_PLATE = ITEMS.register("chainmail_plate", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
 
     public CraftableChainmail() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.spec);
 
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+        ITEMS.register(modEventBus);
 
         Config.loadConfig(FMLPaths.CONFIGDIR.get().resolve(MODID + "-common.toml"));
     }
@@ -51,26 +53,7 @@ public class CraftableChainmail {
         ObfuscationReflectionHelper.setPrivateValue(
                 ArmorMaterial.class,
                 ArmorMaterial.CHAIN,
-                new LazyLoadBase<>(() -> Ingredient.fromItems(CHAINMAIL_PLATE)),
+                new LazyLoadBase<>(() -> Ingredient.fromItems(CHAINMAIL_PLATE.get())),
                 OBSFUCATED_FIELD_NAME_ARMORMATERIAL_REPAIRMATERIAL);
-    }
-
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    private static class RegistryEvents {
-        public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final String name) {
-            return setup(entry, new ResourceLocation(MODID, name));
-        }
-
-        public static <T extends IForgeRegistryEntry<T>> T setup(final T entry, final ResourceLocation registryName) {
-            entry.setRegistryName(registryName);
-            return entry;
-        }
-
-        @SubscribeEvent
-        public static void onRegisterItems(RegistryEvent.Register<Item> e) {
-            e.getRegistry().registerAll(
-                setup(new Item(new Item.Properties().group(ItemGroup.MATERIALS)), "chainmail_plate")
-            );
-        }
     }
 }
